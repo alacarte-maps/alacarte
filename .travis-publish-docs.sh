43056157434cc2691477327829cdf87cea6dc4d1
Florian Jacob <projects+git@florianjacob.de>
2016-04-03 20:49:28 +0200

    pull repo up in configuration variable

--- .travis-publish-docs.sh
+++ .travis-publish-docs.sh
@@ -3,6 +3,7 @@
 # GH_TOKEN is a github personal OAUTH tokens, create it here:
 # https://github.com/settings/tokens/new (only needs “repo“ access.)
 # Then, add them as hidden environment variable in the Travis project settings.
+GH_REPO="alacarte-maps/alacarte"
 
 if [ "${TRAVIS}" = "true" ]; then
 	[ "${TRAVIS_PULL_REQUEST}" = "false" ] || skip "Not building docs for pull requests"
@@ -11,8 +12,9 @@ if [ "${TRAVIS}" = "true" ]; then
 	git config user.name "Travis CI"
 	git config push.default simple
 	ghp-import -n -m "Updated documentation from ${TRAVIS_COMMIT}" doc/doxygen/html
-	git push -fq "https://${GH_TOKEN}@github.com/alacarte-maps/alacarte.git" gh-pages
+	git push -fq "https://${GH_TOKEN}@github.com/${GH_REPO}.git" gh-pages
 	echo "documentation was pushed."
 fi
 
+# script from https://github.com/alacarte-maps/alacarte/blob/master/.travis-publish-docs.sh
 # TODO: it would be nice to be able to publish the docs of multiple branches in subfolders, e.g. master and stable
